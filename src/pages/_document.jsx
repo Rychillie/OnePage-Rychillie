f25319d9247a8a3629850579cfd1856f7e0a9a0b
Rychillie <rychillie@hotmail.com>
2021-04-09 13:18:00 -0300

    google Analytics

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -6,6 +6,22 @@ class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           <link rel="shortcut icon" href="/ok_rychillie.png" />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-45575876-4"
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-45575876-4');
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
